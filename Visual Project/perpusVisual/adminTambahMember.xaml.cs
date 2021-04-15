3da800348195ec4e6c95b7da956ee7949ca6aa59
kennethliem <kennethliem991@gmail.com>
2021-04-15 14:47:20 +0700

    Design

---
+++ Visual Project/perpusVisual/adminTambahMember.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace perpusVisual
+{
+    /// <summary>
+    /// Interaction logic for adminTambahMember.xaml
+    /// </summary>
+    public partial class adminTambahMember : Window
+    {
+        public adminTambahMember()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Visual Project/perpusVisual/selfPeminjaman.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace perpusVisual
+{
+    /// <summary>
+    /// Interaction logic for selfPeminjaman.xaml
+    /// </summary>
+    public partial class selfPeminjaman : Window
+    {
+        public selfPeminjaman()
+        {
+            InitializeComponent();
+        }
+    }
+}
