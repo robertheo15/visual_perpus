7dc8df05620b7d1825492b86892dc8a93c96e619
kennethliem <kennethliem991@gmail.com>
2021-04-19 10:59:38 +0700

    Pindah Page ( Sementara )

--- Visual Project/perpusVisual/MainWindow.xaml.cs
+++ Visual Project/perpusVisual/MainWindow.xaml.cs
@@ -24,5 +24,49 @@ namespace perpusVisual
         {
             InitializeComponent();
         }
+
+        private void btnPinjam(object sender, RoutedEventArgs e)
+        {
+            adminPeminjaman pinjam = new adminPeminjaman();
+            pinjam.Show();
+            this.Hide();
+        }
+
+        private void btnKembalian(object sender, RoutedEventArgs e)
+        {
+            adminPengembalian kembalian = new adminPengembalian();
+            kembalian.Show();
+            this.Hide();
+        }
+        private void btnDataBuku(object sender, RoutedEventArgs e)
+        {
+            adminDataBuku dataBuku = new adminDataBuku();
+            dataBuku.Show();
+            this.Hide();
+        }
+        private void btnDataMember(object sender, RoutedEventArgs e)
+        {
+            adminDataMember dataMember = new adminDataMember();
+            dataMember.Show();
+            this.Hide();
+        }
+        private void btnDataPinjam(object sender, RoutedEventArgs e)
+        {
+            adminDataPinjam dataPinjam = new adminDataPinjam();
+            dataPinjam.Show();
+            this.Hide();
+        }
+        private void btnLaporan(object sender, RoutedEventArgs e)
+        {
+            adminLaporan laporan = new adminLaporan();
+            laporan.Show();
+            this.Hide();
+        }
+        private void btnLogout(object sender, RoutedEventArgs e)
+        {
+            selfMainWindows logout = new selfMainWindows();
+            logout.Show();
+            this.Close();
+        }
     }
 }
--- Visual Project/perpusVisual/adminDataPinjam.xaml.cs
+++ Visual Project/perpusVisual/adminDataPinjam.xaml.cs
@@ -21,5 +21,55 @@ namespace perpusVisual
         {
             InitializeComponent();
         }
+
+        private void btnPinjam(object sender, RoutedEventArgs e)
+        {
+            adminPeminjaman pinjam = new adminPeminjaman();
+            pinjam.Show();
+            this.Close();
+        }
+
+        private void btnKembalian(object sender, RoutedEventArgs e)
+        {
+            adminPengembalian kembalian = new adminPengembalian();
+            kembalian.Show();
+            this.Close();
+        }
+        private void btnDataBuku(object sender, RoutedEventArgs e)
+        {
+            adminDataBuku dataBuku = new adminDataBuku();
+            dataBuku.Show();
+            this.Close();
+        }
+        private void btnDataMember(object sender, RoutedEventArgs e)
+        {
+            adminDataMember dataMember = new adminDataMember();
+            dataMember.Show();
+            this.Close();
+        }
+        private void btnDataPinjam(object sender, RoutedEventArgs e)
+        {
+            adminDataPinjam dataPinjam = new adminDataPinjam();
+            dataPinjam.Show();
+            this.Close();
+        }
+        private void btnLaporan(object sender, RoutedEventArgs e)
+        {
+            adminLaporan laporan = new adminLaporan();
+            laporan.Show();
+            this.Close();
+        }
+        private void btnLogout(object sender, RoutedEventArgs e)
+        {
+            selfMainWindows logout = new selfMainWindows();
+            logout.Show();
+            this.Close();
+        }
+        private void btnKembali(object sender, RoutedEventArgs e)
+        {
+            MainWindow back = new MainWindow();
+            back.Show();
+            this.Close();
+        }
     }
 }
