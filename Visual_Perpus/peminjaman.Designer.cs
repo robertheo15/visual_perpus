3895674ebcd1317d4ae80ec0c4426579060cf788
robertheo <robertheo.rt@gmail.com>
2021-05-18 12:51:59 +0700

    ok

--- Visual_Perpus/peminjaman.Designer.cs
+++ Visual_Perpus/peminjaman.Designer.cs
@@ -47,6 +47,10 @@ namespace Visual_Perpus
             this.panel1 = new System.Windows.Forms.Panel();
             this.TxtBoxErrorNim = new System.Windows.Forms.TextBox();
             this.TxtBoxErrorBook = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.bunifuCustomDataGrid1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -162,7 +166,7 @@ namespace Visual_Perpus
             this.bunifuCustomDataGrid1.AllowUserToDeleteRows = false;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.bunifuCustomDataGrid1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            this.bunifuCustomDataGrid1.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.bunifuCustomDataGrid1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.bunifuCustomDataGrid1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.bunifuCustomDataGrid1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
@@ -275,14 +279,62 @@ namespace Visual_Perpus
             this.TxtBoxErrorBook.TabIndex = 21;
             this.TxtBoxErrorBook.TextChanged += new System.EventHandler(this.TxtBoxErrorBook_TextChanged);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(79, 437);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "NIM :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label8.Location = new System.Drawing.Point(80, 475);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 25);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Nama :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label9.Location = new System.Drawing.Point(582, 475);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(273, 25);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Maksimal Lama Peminjaman :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label10.Location = new System.Drawing.Point(582, 437);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(269, 25);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Maksimal Peminjaman Buku :";
+            // 
             // Peminjaman
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1130, 677);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.TxtBoxErrorBook);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.TxtBoxErrorNim);
+            this.Controls.Add(this.label10);
             this.Controls.Add(this.BtnPinjamBuku);
             this.Controls.Add(this.TxtBoxIdBookPeminjaman);
             this.Controls.Add(this.label13);
@@ -321,5 +373,9 @@ namespace Visual_Perpus
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox TxtBoxErrorNim;
         private System.Windows.Forms.TextBox TxtBoxErrorBook;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
